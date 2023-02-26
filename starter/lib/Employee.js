9c601be549824d6c28d45e0c7564f93fe85570e5
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-02-26 19:34:00 +0000

    added Employee class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+export default class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName(){
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+    getId() {
+        return this.email;
+    }
+
+    getRole(){
+        return "Employee";
+    }
+}
