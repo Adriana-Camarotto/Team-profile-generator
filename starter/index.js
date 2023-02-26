c582eba49200fef70bf8ad5f31c105b098a037ec
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-02-26 22:45:31 +0000

    create functions do prompt team details, to adding new members or finishing building the team

--- starter/index.js
+++ starter/index.js
@@ -16,3 +16,142 @@ const render = require("./src/page-template.js");
 // Create an empty array to hold the team members
 const teamMembers = [];
 
+//function to prompt for manager details
+function promptManager() {
+    return inquirer 
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email?",        
+        },
+        {
+            tyoe: "input",
+            name: "officeNumber",
+            message: "what is the team manager`s office number?",
+        }
+    ])
+    .then((answers) => {
+        const manager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.officeNumber
+        );
+        teamMembers.push(manager);
+        console.log("Manager added successfully!");  
+    });
+}
+
+// function to prompt for engineer details
+function promptEngineer() {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's email?",        
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "what is the engineer's github username?",
+        },        
+    ])
+    .then((answers) => {
+    
+    const engineer = new Engineer(
+        answers.name,
+        answeres.id,
+        answeres.email,
+        answers.github
+        );
+        teamMembers.push(engineer);
+        console.log("Engineer added successfully!");
+    }); 
+}
+
+//function to prompt for intern details
+function promptIntern() {
+    return inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's ID?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email?",        
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "what is the intern's github username?",
+        },        
+    ])
+    .then((answers) => {
+    const intern = new Intern(
+        answers.name,
+        answeres.id,
+        answeres.email,
+        answers.school
+        );
+        teamMembers.push(intern);
+        console.log("Intern added successfully!");
+    }); 
+}
+
+//function to prompt for adding another team member or finishing building the team
+function promptTeam() {
+    return inquirer
+    .prompt({
+        {
+            type: "list",
+            name: "action",
+            message: "What woul you like to do next?",
+            choices: [
+                "Add an engineer",
+                "Add an intern",
+                "finish building the team",
+            ],
+        },
+    })
+    .then((answers) => {
+        switch (answers.action){
+            case "add an engineer":
+                return promptEnginner().then(() => promptTeam());
+            case "Add an intern":
+                return promptIntern().then(() => promptTeam());
+            default:
+                console.log("Team created successfully!");
+                return;
+        }
+    ));
+}
